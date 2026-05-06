20d6e5e916b4f34cd3d7e527a5701db74f36e639
Mateus <mateussab1@gmail.com>
2026-05-06 19:32:30 -0300

    commit inicial com esqueleto

---
+++ src/test/java/com/mateus/bffagendador/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.bffagendador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
